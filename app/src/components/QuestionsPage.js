d6961006fd11752ffad02c99260c2cb8efd1371b
marvin lewis <marvinlewis01@gmail.com>
2020-03-22 19:48:31 -0700

    added loader

--- app/src/components/QuestionsPage.js
+++ app/src/components/QuestionsPage.js
@@ -2,32 +2,40 @@ import React from "react";
 import { connect } from "react-redux";
 import { getQuestions } from "./reducer/actions/actions";
 import Questions from "./Questions";
+import Loader from 'react-loader-spinner'
+import "./loading.css";
 
 const QuestionsPage = (props) => {
 
     return (
         <div>
-            <button onClick={props.getQuestions}>
+            <button className="questions" onClick={props.getQuestions}>
                 Questions
             </button>
-            {props.isLoading && <h2>Loading ...</h2>}
+            {props.isLoading && <Loader
+                type="Puff"
+                color="#00BFFF"
+                height={100}
+                width={100}
+                timeout={3000}
+            />}
             {props.questions.map(question => {
-               //console.log(question)
+                //console.log(question)
                 return (
-                    <Questions 
-                    key={Date.now()}
-                    question={question} />    
+                    <Questions
+                        key={Date.now()}
+                        question={question} />
                 )
-            })}  
+            })}
         </div>
     )
 }
 
 const mapStateToProps = state => {
     return {
-        questions : state.questions,
-        isLoading : state.isLoading,
-        error : state.error,
+        questions: state.questions,
+        isLoading: state.isLoading,
+        error: state.error,
     }
 }
 
